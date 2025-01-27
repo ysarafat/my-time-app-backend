a53ec7755c1b60fed0bd1900de250749edf7ccb5
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-01-27 20:10:47 +0600

    refactor admin service to enhance search functionality with dynamic filtering

--- src/app/modules/admin/admin.services.ts
+++ src/app/modules/admin/admin.services.ts
@@ -1,31 +1,38 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 // get admins
 const getAdmins = async (query: Record<string, any>) => {
+  const filtering: Prisma.AdminWhereInput[] = [];
+  const searchableFields = ["name", "email", "phone"];
+  const { search, ...filterBy } = query;
+  if (search) {
+    filtering.push({
+      OR: searchableFields.map((field) => ({
+        [field]: {
+          contains: search,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+  if (Object.keys(filterBy).length > 0) {
+    filtering.push({
+      AND: Object.entries(filterBy).map(([key, value]) => {
+        console.log(key, value);
+        return {
+          [key]: {
+            equals: value,
+          },
+        };
+      }),
+    });
+  }
+
+  const whereCondition: Prisma.AdminWhereInput = { AND: filtering };
   const admins = await prisma.admin.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: query.search,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: query.search,
-            mode: "insensitive",
-          },
-        },
-        {
-          phone: {
-            contains: query.search,
-          },
-        },
-      ],
-    },
+    where: whereCondition,
   });
   return admins;
 };
